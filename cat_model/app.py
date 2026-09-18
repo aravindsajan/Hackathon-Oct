33e18600ad327393802e8e7ddd842569f8028f9e
Aravind Sajan <87465751+aravindsajan@users.noreply.github.com>
2026-09-19 02:49:28 +0530

    Update app.py

--- cat_model/app.py
+++ cat_model/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import tensorflow as tf
 import pickle
 import numpy as np
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 app = Flask(__name__)
+CORS(app)
 
 model = tf.keras.models.load_model("news_category_model.keras")
 
@@ -14,9 +16,12 @@ with open("tokenizer.pkl", "rb") as f:
 with open("label_encoder.pkl", "rb") as f:
     encoder = pickle.load(f)
 
+@app.route("/")
+def home():
+    return "News Category API is running!"
+
 @app.route("/predict-category", methods=["POST"])
 def predict_category():
-
     data = request.get_json()
 
     news = data["news"]
@@ -34,13 +39,9 @@ def predict_category():
 
     predicted_class = np.argmax(prediction[0])
 
-    category = encoder.inverse_transform(
-        [predicted_class]
-    )[0]
+    category = encoder.inverse_transform([predicted_class])[0]
 
-    confidence = float(
-        np.max(prediction[0])
-    )
+    confidence = float(np.max(prediction[0]))
 
     return jsonify({
         "category": category,
@@ -48,4 +49,8 @@ def predict_category():
     })
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=False
+    )
